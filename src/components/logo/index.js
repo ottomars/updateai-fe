297be5deb296b011f2a10fd5cb743022bc45c92f
Jed Richards <jedrichards@gmail.com>
2016-10-13 12:13:35 +0100

    Styling...

--- src/components/logo/index.js
+++ src/components/logo/index.js
@@ -1,8 +1,12 @@
 import './index.css'
 import React from 'react'
+import logoMedia from './logo.jpg'
 
 const Logo = ({children}) => (
-  <a className='Logo'>Update.AI</a>
+  <a className='Logo'>
+    <img src={logoMedia} className='Logo-media' role='presentation' />
+    <p className='Logo-text'>UpdateAI</p>
+  </a>
 )
 
 export default Logo
--- src/components/search/index.js
+++ src/components/search/index.js
@@ -37,7 +37,7 @@ const Search = ({dataSource, onNewRequest}) => (
       fullWidth={true}
       menuCloseDelay={0}
       filter={AutoComplete.fuzzyFilter}
-      underlineFocusStyle={{borderColor: '#f558bf'}}
+      underlineFocusStyle={{borderColor: '#1f5acc'}}
       onNewRequest={onNewRequest}
     />
   </div>
